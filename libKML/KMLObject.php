1df2f9c1704391a7414b8bf38efff8dc297ee09b
Xavier Carriba <xavier.carriba@gmail.com>
2012-04-10 23:43:25 +0200

    Doxygen documentation

--- libKML/KMLObject.php
+++ libKML/KMLObject.php
@@ -3,14 +3,21 @@ namespace libKML;
 
 /**
  *  KMLObject abstract class
+ *  Base KMLObject
  */
 abstract class KMLObject {
   protected $id;
   
+  /**
+   *  Sets KML tag id
+   */
   public function setId($id) {
     $this->id = $id;
   }
   
+  /**
+   *  Gets KML tag id
+   */
   public function getId() {
     return $this->id;
   }
