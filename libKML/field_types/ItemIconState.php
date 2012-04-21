07d1ad5d3959573a8c5268a441faedf9512e6e80
xavier <xavier@10.0.0.20>
2012-04-21 21:50:07 +0200

    Fix id attribute issue

---
+++ libKML/field_types/ItemIconState.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ItemIconState
+ *
+ * @author xavier
+ */
+class ItemIconState {
+  
+}
+
+?>
